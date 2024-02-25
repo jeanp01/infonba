7f90e1c25d2f45155a99eb95ee5123c65015e8fa
Jean Portilla <jeanportilla01@gmail.com>
2024-02-25 11:20:42 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [theme, setTheme] = useState(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      return "dark";
+    }
+
+    return "light";
+  });
+
+  useEffect(() => {
+    if (theme == "dark") {
+      document.querySelector("html").classList.add("dark");
+    } else {
+      document.querySelector("html").classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleChangeTheme = () => {
+    setTheme((prevTheme) => (prevTheme == "light" ? "dark" : "light"));
+  };
+
+  return (
+    <main>
+      <div className="h-screen flex justify-center items-center dark:bg-neutral-900">
+        <button
+          className="bg-slate-200 px-4 py-2 hover:bg-slate-300 dark:bg-slate-950 dark:text-white dark:hover:bg-slate-900"
+          onClick={handleChangeTheme}
+        >
+          change theme
+        </button>
+      </div>
+    </main>
+  );
+}
+
+export default App;
